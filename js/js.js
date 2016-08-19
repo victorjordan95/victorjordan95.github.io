376987c6375614f894d5d43d082caaf2c903f75c
Victor Jordan <victorjordan95@gmail.com>
2016-08-19 00:01:25 -0300

    no message

---
+++ js/js.js
@@ -0,0 +1,37 @@
+		$( ".cog" ).on( "click", function(){
+				$( ".menu" ).stop().fadeToggle( "0.5" );
+			});
+			
+			$( ".menu-link" ).on( "click", function(){
+				$( ".menu" ).stop().fadeOut( "fast" );
+			});
+			$( "header, .section-zallium" ).on( "click", function(){
+				$( ".menu" ).fadeOut( "fast" );
+			});
+
+			$(document).ready(function () {
+			    function filterPath(string) {
+			        return string
+			            .replace(/^\//, '')
+			            .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+			            .replace(/\/$/, '');
+			    }
+			    $('a[href*=#]').each(function () {
+			        if (filterPath(location.pathname) == filterPath(this.pathname)
+			                && location.hostname == this.hostname
+			                && this.hash.replace(/#/, '')) {
+			            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+			            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+			            if ($target) {
+			                var targetOffset = $target.offset().top;
+			                $(this).click(function () {
+			                    if($(window).width() > 200){
+			                        $('html, body').animate({scrollTop: targetOffset}, 800);
+			                        return false;
+			                    }
+			                });
+			            }
+			        }
+			    });
+			});
+			
