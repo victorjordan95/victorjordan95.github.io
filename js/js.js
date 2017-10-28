3942b1d85633fb0e595e7517c2467f25921042c0
Victor Jordan <victorjordan95@gmail.com>
2017-10-28 17:50:22 -0200

    criando novo portfolio

---
+++ js/js.js
@@ -0,0 +1,41 @@
+// $("#owl-clients").owlCarousel({
+//     autoplay: true,
+//     autoplayHoverPause: true,
+//     loop: true,
+//     margin: 20,
+//     responsiveClass: true,
+//     nav: true,
+//     loop: true,
+//     navText: ["<svg class='icone icon-arrow-left'><use xlink:href='#icon-arrow-left'></use></svg>", "<svg class='icone icon-arrow-right'><use xlink:href='#icon-arrow-right'></use></svg>"],
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         568: {
+//             items: 2
+//         },
+//         600: {
+//             items: 3
+//         },
+//         1000: {
+//             items: 4
+//         }
+//     }
+// });
+
+var menuClass = document.getElementById("js-open-menu").classList;
+var menu = document.getElementById("js-open-menu");
+
+function openMenu(){
+    if(!menuClass.contains("closed") && !menuClass.contains("active")){
+        menuClass.add('active');
+    } else {
+        if(menuClass.contains("active")){
+            menuClass.remove("active");
+            menuClass.add("closed")
+        } else {
+            menuClass.remove('closed');
+            menuClass.add('active');
+        }
+    }
+}
